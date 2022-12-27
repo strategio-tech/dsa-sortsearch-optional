91ef63c62e8677b12cffa9bb16988690c27b8d11
Deeps <100620410+deepti-tri@users.noreply.github.com>
2022-12-27 11:35:45 -0600

    feat: first commit

---
+++ src/main/java/tech/strategio/E01.java
@@ -0,0 +1,16 @@
+package tech.strategio;
+
+public class E01 {
+
+    /**
+     * Write a function that takes in an array of integers and returns a sorted version of that array.
+     * Use the Bubble Sort algorithm to sort the array.
+     *
+     * @param array The array of numbers
+     * @return The sorted array
+     */
+    public static int[] bubbleSort(int[] array) {
+        // @TODO Write out the method
+        return new int[]{};
+    }
+}
---
+++ src/test/java/tech/strategio/TestE01.java
@@ -0,0 +1,29 @@
+package tech.strategio;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+
+public class TestE01 {
+    @DisplayName("checkIfArrayIsSorted")
+    @ParameterizedTest(name = "{index} => {0} is {1}")
+    @MethodSource("argumentStream")
+    void argumentStream(int[] values, int[] result) {
+        int[] val = E01.bubbleSort(values);
+        assertArrayEquals(result, val);
+    }
+
+    private static Stream<Arguments> argumentStream() {
+        return Stream.of(
+                Arguments.arguments(new int[] { 141, 1, 17, -7, -17, -27 },
+                        new int[] { -27, -17, -7, 1, 17, 141 }),
+                Arguments.arguments(new int[] { 55, 7, 8 }, new int[] { 7, 8, 55 }),
+                Arguments.arguments(new int[] { 55, 43, 11, 0, 3, -3, 10 }, new int[] { -3, 0, 3, 10, 11, 43, 55 }),
+                Arguments.arguments(new int[] { 7, 7, 7, 7, 7 }, new int[] { 7, 7, 7, 7, 7 }),
+                Arguments.arguments(new int[] { -1, -2, -3, -7, -17, -27 },
+                        new int[] { -27, -17, -7, -3, -2, -1 }));
+    }
+}
